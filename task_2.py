bf11f379385c09bafa8ed8c70f867a6c9f241db2
Alexander <alek.agapow80@yandex.ru>
2022-04-19 20:49:54 +0700

    решили задачу 2

---
+++ task_2.py
@@ -0,0 +1,28 @@
+
+2.	#Найти максимальное из пяти чисел
+max=0
+min=0
+a=1
+b=893
+c=100
+g=34
+h=50
+if a>b:
+   max=a
+else:  
+    max=b
+if max>c:
+   c=min
+else:  
+    max=c
+if max>g:
+   min=g
+else:  
+    max=g
+if max>h:
+   min=h
+else:  
+    max=h
+
+print('максимальное число =', max)
+
