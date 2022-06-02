3303bb933f33f5e80a701accacce08b75e0e65a8
Alexander <alek.agapow80@yandex.ru>
2022-06-02 11:29:00 +0700

    непонятное условие

---
+++ task_17.py
@@ -0,0 +1,24 @@
+# 	Задать список из N элементов, заполненных числами из [-N, N]. 
+# Найти произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в 
+# одной строке одно число
+def range_list(a,b):
+ numbert =list(range(a,b))
+ return numbert
+
+
+# #def mult_spisok(line1,line2):
+ 
+# from os import close
+
+
+# file=open(r'E:\0\001\2022\IT учеба\Python\Home task\file17.txt','r')
+# cont = file.read()
+# print(cont)
+
+
+
+s=int(input(' введите первое число диапазона  = '))
+k=int(input(' введите второе число диапазона  = '))
+# c=range_list(s,k)
+# print(f, cgbcjdr, c)
+
